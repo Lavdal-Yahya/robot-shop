f1f60c0e23d8d3851fb36c688700ae87325a0566
Lavdal <LavdalYahya@gmail.com>
2025-03-13 11:17:52 +0000

    Added SignIn through Template Forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
 import { filter } from 'rxjs';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent, title: "home - Robot shop"},
   {path: 'catalog', component: CatalogComponent, title: "catalog - Robot shop"},
   {path: 'cart', component: CartComponent, title: "cart - Robot shop"},
+  {path: 'sign-in', component: SignInComponent, title: "Sign In - Robot shop"},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
 
--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from '../user/user.model';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'bot-site-header',
   templateUrl: './site-header.component.html',
   styleUrls: ['./site-header.component.css']
 })
-export class SiteHeaderComponent {
+export class SiteHeaderComponent implements OnInit{
+  user: IUser | null = null;
+  showSignoutMenu: boolean = false;
 
-  constructor() { }
+  constructor(private userSvc: UserService) { }
 
+  ngOnInit() {
+    this.userSvc.getUser().subscribe({
+      next: (user) => {this.user = user}
+    });
+  }
+
+  toggleSignOutMenu (){
+    this.showSignoutMenu = !this.showSignoutMenu;
+  }
+
+  signOut() {
+    this.userSvc.signOut();
+    this.showSignoutMenu = false;
+  }
 }
